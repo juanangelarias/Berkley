947486639de0908752cb0e3557d16fa209c9844a
Bryan Ulfers <bulfers@berkleysurety.com>
2024-02-14 15:01:48 -0600

    Added GraphQL

---
+++ JamesWebUI/Client/Classes/Notification.cs
@@ -0,0 +1,9 @@
+﻿namespace JamesWebUI.Client.Classes
+{
+    public class Notification
+    {
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Content { get; set; }
+    }
+}

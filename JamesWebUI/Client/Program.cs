a969d16ede0d339d6eb58859f00fa7a9f84a3150
Bryan Ulfers <bulfers@berkleysurety.com>
2024-02-15 12:53:57 -0600

    Updated Client to .NET 8 structure

--- JamesWebUI/Client/Program.cs
+++ JamesWebUI/Client/Program.cs
@@ -3,20 +3,19 @@ using JamesWebUI.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("JamesAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
     .CreateClient("JamesAPI"));
-
-
-
+builder.Services.AddRadzenComponents();
 builder.Services.AddTransient<ThemeService>();
+
+
 
 builder.Services.AddOidcAuthentication(options =>
 {
--- JamesWebUI/Server/Program.cs
+++ JamesWebUI/Server/Program.cs
@@ -1,9 +1,14 @@
 using James.Data.Server.Model;
+using JamesWebUI.Client;
+using JamesWebUI.Client.Components;
+using JamesWebUI.Client.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Radzen;
+
 
 
 var config = new ConfigurationBuilder()
@@ -19,6 +24,7 @@ builder.Services.AddHttpClient("Auth0UserInfo",
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
     .CreateClient("Auth0UserInfo"));
 
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -29,9 +35,12 @@ builder.Services.AddAuthentication(options =>
     options.Audience = builder.Configuration["Auth0:ApiIdentifier"];
 });
 
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
 builder.Services
     .AddPooledDbContextFactory<JamesDatabaseContext>(o =>
     {
@@ -75,20 +84,21 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization(); // Authorization ALWAYS after Authentication, both after UseRouting(); 
+app.UseAntiforgery();
 
 app.UseCors();
 
 
 app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode();
 
 app.MapGraphQL("/graphql");
 

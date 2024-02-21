771e4ca6f46d67a2d3e5c9788578fe5f3c6e813b
Bryan Ulfers <bulfers@berkleysurety.com>
2024-02-21 12:58:47 -0600

    Merge

---
+++ JamesWebUI/Server/GraphQL/Queries/Agency.cs
@@ -0,0 +1,11 @@
+﻿using James.Data.Server.Model;
+using James.Shared.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace JamesWebUI.Server.GraphQL.Queries
+{
+    public partial class Query
+    {
+        
+    }
+}
---
+++ JamesWebUI/Server/GraphQL/Queries/State.cs
@@ -0,0 +1,15 @@
+﻿using James.Data.Server.Model;
+using James.Shared.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace JamesWebUI.Server.GraphQL.Queries
+{
+    public partial class Query
+    {
+        public List<State> GetAllStates([Service] IDbContextFactory<JamesDatabaseContext> contextFactory)
+        {
+            var ctx = contextFactory.CreateDbContext();
+            return ctx.States.ToList();
+        }
+    }
+}

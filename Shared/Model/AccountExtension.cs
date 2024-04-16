5195aee40c854b9ba3823f4725cfb8476cb65203
Bryan Ulfers <bulfers@berkleysurety.com>
2024-04-16 15:08:33 -0500

    Adding file

---
+++ Shared/Model/AccountExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace James.Shared.Model
+{
+    public partial class Account
+    {
+        public Address? MainAddress
+        {
+            get
+            {
+                return this.IdNavigation?.LegalEntityAddresses?.Where(a => a.Type == "Main")?.FirstOrDefault()?.Address;
+            }
+            set
+            {
+                this.MainAddress = value;
+            }
+        }
+    }
+}

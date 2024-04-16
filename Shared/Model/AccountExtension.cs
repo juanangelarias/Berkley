eccf8008f7eae56aec3eff99475e2da4b970653e
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-04-16 15:27:30 -0500

    Snippet for Bryan

--- Shared/Model/AccountExtension.cs
+++ Shared/Model/AccountExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,27 @@ namespace James.Shared.Model
             }
             set
             {
-                this.MainAddress = value;
+                //UNDONE: Below is sample code for Bryan to clean up an finish
+                var lea = IdNavigation.LegalEntityAddresses.SingleOrDefault(l=>l.Type== "Main");
+                //TODO: handle value == null
+                Debug.Assert(value != null, nameof(value) + " != null");
+                if (null == lea)
+                {
+                    if ((value?.Id??Guid.Empty) == Guid.Empty)
+                    {
+                        value.Id  =  Guid.NewGuid();
+                    }
+
+                    var newLea = new LegalEntityAddress
+                    {
+                        Type = "Main",
+                        AddressId = value.Id,
+                        Address = value
+                    };
+                    lea = newLea;
+                }
+
+                lea.Address = value;
             }
         }
     }

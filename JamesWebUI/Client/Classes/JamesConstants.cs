ae16341a5d5f3779f44338d978ee6bc87c77f0bb
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-06-13 15:59:05 -0500

    Extracted constant for log in and log out paths

---
+++ JamesWebUI/Client/Classes/JamesConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace JamesWebUI.Client.Classes
+{
+    public static class JamesConstants
+    {
+        public const string LOG_IN_PATH = "/account/Login";
+        public const string LOG_OUT_PATH = "/account/Logout";
+    }
+}

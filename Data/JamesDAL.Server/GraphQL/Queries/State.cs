d1f5407e2b7b966a49bced10e0c56c11e2e412f4
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-06-17 08:43:30 -0500

    Minor refactoring, moving constants to James.Shared

--- Data/JamesDAL.Server/GraphQL/Queries/State.cs
+++ Data/JamesDAL.Server/GraphQL/Queries/State.cs
@@ -2,6 +2,12 @@
 {
     public partial class Query
     {
+        /// <summary>
+        /// Get all states from our states table
+        /// </summary>
+        /// <param name="contextFactory">service</param>
+        /// <returns>All states in out database</returns>
+        /// <remarks>Does not require authorizations</remarks>
         public async Task<List<State>> GetAllStates([Service] IDbContextFactory<JamesDatabaseContext> contextFactory)
         {
             var ctx = await contextFactory.CreateDbContextAsync();

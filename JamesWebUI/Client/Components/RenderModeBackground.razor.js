64bc10da42e61801d6cb76dee8263eed2ba1b1f4
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-06-17 15:22:11 -0500

    Added component to change background color pased on whether it is rendered on client or server to help troubleshooting.

---
+++ JamesWebUI/Client/Components/RenderModeBackground.razor.js
@@ -0,0 +1,21 @@
+﻿function getParentDiv(el){
+    while (el && el.parentNode) {
+        el = el.parentNode;
+        if (el.tagName && el.tagName.toLowerCase() == 'div') {
+            return el;
+        }
+    }
+    return null;
+}
+function setBackground(el, color){
+    var pdiv = getParentDiv(el);
+    if (pdiv)
+        pdiv.style.backgroundColor = color;
+}
+export function setBackgroundClient(el){
+    setBackground(el, "aliceblue");
+}
+export function setBackgroundServer(el){
+    setBackground(el, "mistyrose");
+}
+

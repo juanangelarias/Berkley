7fecfa447b2eb6269c2ffd6bbd6f4bd68a817306
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-06-27 11:27:18 -0500

    Adding documentation via comments

--- Shared/Model/ITieredValue.cs
+++ Shared/Model/ITieredValue.cs
@@ -4,6 +4,15 @@
     {
         public int Minimum { get; set; }
         public int? Maximum { get; set; }
+        /// <summary>
+        /// This value for the tier
+        /// </summary>
+        /// <remarks>This is typically a reference to an existing property.
+        /// If the concrete object is an entity object used in GraphQL, the
+        /// object will need a Value property added in a partial class file
+        /// that is decorated with a [GraphQLIgnore] and [NotMapped] attributes
+        /// to cause it to be ignored by Entities Framework Core and HotChocolate
+        /// respectively.</remarks>
         public T Value { get; set; }
     }
 }

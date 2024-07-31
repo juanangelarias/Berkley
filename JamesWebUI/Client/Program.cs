ea7ab5c2a636183fe92cea44c3119558380b89e6
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-07-31 11:44:56 -0500

    Minor cleanup

--- JamesWebUI/Client/Program.cs
+++ JamesWebUI/Client/Program.cs
@@ -2,11 +2,9 @@ using James.Data.Client;
 using James.Data.Client.GraphQL;
 using James.Shared;
 using James.Shared.Data;
-using JamesWebUI.Client;
-using JamesWebUI.Client.Services;
 using JamesWebUI.Client.AuthenticationStateSyncer;
+using JamesWebUI.Client.Services;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 using StrawberryShake;
@@ -17,12 +15,8 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
-builder.Services.AddHttpClient("JamesAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    //.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>()
-    ;
-builder.Services.AddHttpClient(JamesClient.ClientName, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    //.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>()
-    ;
+builder.Services.AddHttpClient("JamesAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient(JamesClient.ClientName, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 var graphqlHttpUrl = builder.HostEnvironment.BaseAddress + "graphql";
 var graphqlWebSocketUrl = graphqlHttpUrl.Replace("http", "ws", StringComparison.InvariantCultureIgnoreCase);
@@ -33,12 +27,10 @@ builder.Services.AddJamesClient(ExecutionStrategy.CacheAndNetwork)
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
     .CreateClient("JamesAPI"));
 
-
 builder.Services.AddRadzenComponents();
 builder.Services.AddScoped<ThemeService>();
 builder.Services.AddSingleton<ILoggingService, LoggingService>();
 builder.Services.AddScoped<IDataAccess,ClientDataAccess>();
-
 
 builder.Services.AddOidcAuthentication(options =>
 {

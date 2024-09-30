b737354a95e0173741b044d8a3b5d6e8f58e3178
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-09-30 10:41:55 -0500

    Virtual File UX work

--- Tests/JamesWebUI.Client.Test/Extensions.cs
+++ Tests/JamesWebUI.Client.Test/Extensions.cs
@@ -17,5 +17,40 @@ namespace JamesWebUI.Client.Test
             Assert.Equal("<BLANK>", "".ToScreenText());
             Assert.Equal("<NULL>", ((string?)null).ToScreenText());
         }
+
+        [Fact]
+        public void ToScreenBytes1()
+        {
+            Assert.Equal("1B", 1L.ToScreenBytes());
+            Assert.Equal("1.0KB", 1024L.ToScreenBytes());
+            Assert.Equal("1.0MB", ((long)Math.Pow(1024L, 2)).ToScreenBytes());
+            Assert.Equal("1.0GB", ((long)Math.Pow(1024L, 3)).ToScreenBytes());
+            Assert.Equal("1.0TB", ((long)Math.Pow(1024L, 4)).ToScreenBytes());
+            Assert.Equal("1.0PB", ((long)Math.Pow(1024L, 5)).ToScreenBytes());
+            Assert.Equal("1.0EB", ((long)Math.Pow(1024L, 6)).ToScreenBytes());
+        }
+
+        [Fact]
+        public void ToScreenBytes2()
+        {
+            Assert.Equal("100B", 100L.ToScreenBytes());
+            Assert.Equal("256.0KB", (256L*1024L).ToScreenBytes());
+            Assert.Equal("25.6MB", ((long)(25.6*Math.Pow(1024L, 2))).ToScreenBytes());
+            Assert.Equal("25.6GB", ((long)(25.6*Math.Pow(1024L, 3))).ToScreenBytes());
+            Assert.Equal("25.6TB", ((long)(25.6*Math.Pow(1024L, 4))).ToScreenBytes());
+            Assert.Equal("25.6PB", ((long)(25.6*Math.Pow(1024L, 5))).ToScreenBytes());
+            Assert.Equal("2.1EB", ((long)(2.1*Math.Pow(1024L, 6))).ToScreenBytes());
+        }
+
+        [Fact]
+        public void ToScreenBytes3()
+        {
+            Assert.Equal("1010B", 1010L.ToScreenBytes());
+            Assert.Equal("1010.0KB", (1010L*1024L).ToScreenBytes());
+            Assert.Equal("1010.0MB", (1010L*(long)Math.Pow(1024L, 2)).ToScreenBytes());
+            Assert.Equal("1010.0GB", (1010L*(long)Math.Pow(1024L, 3)).ToScreenBytes());
+            Assert.Equal("1010.0TB", (1010L*(long)Math.Pow(1024L, 4)).ToScreenBytes());
+            Assert.Equal("1010.0PB", (1010L*(long)Math.Pow(1024L, 5)).ToScreenBytes());
+        }
     }
 }
--- XCopyDeployTemplate.cmd
+++ XCopyDeployTemplate.cmd
@@ -26,12 +26,14 @@ SET DestPath=%server%%DestinationPath%
 ECHO Destination path = %DestPath%
 
 ECHO Compiling and Publishing
+dotnet restore
 c:
 pushd JamesWebUI\Server
 REM Choose debug or release for the environment
 SET Env=debug
 dotnet publish -c %Env%  --self-contained -nologo -f %NetVersion% -r win-x64
 
+:BackupAppSettingsJson
 if "%server:~0,5%" neq "wrbts" (
 	ECHO Deploying to %server%
 	ECHO Stopping services
@@ -40,7 +42,7 @@ if "%server:~0,5%" neq "wrbts" (
 	sc \\%server:"=% stop JamesTheBondSystem
 
 	ECHO Backing up appsettings.json files to "\\%DestPath%%DestinationSubFolder%\appsettings.json" and "\\%DestPath%%DestinationSubFolder%\client.appsettings.json"
-	robocopy "\\%DestPath%%DestinationSubFolder%\" "\\%DestPath%\" appsettings.json /w:5 /r:100000
+	robocopy "\\%DestPath%%DestinationSubFolder%\." "\\%DestPath%\." appsettings.json /w:5 /r:100000
 	copy /Y "\\%DestPath%%DestinationSubFolder%\wwwroot\appsettings.json" /A \\$DestPath\client.appsettings.json /A
 )
 
@@ -51,7 +53,7 @@ popd
 
 if /I "%server:~0,5%" neq "wrbts" (
 	ECHO Restoring appsettings.json files
-	robocopy "\\%DestPath%\." "\\%DestPath%%DestinationSubFolder%" appsettings.json /w:5 /r:100000
+	robocopy "\\%DestPath%\." "\\%DestPath%%DestinationSubFolder%\." appsettings.json /w:5 /r:100000
 	copy /Y \\$DestPath\client.appsettings.json /A "\\%DestPath%%DestinationSubFolder%\wwwroot\appsettings.json" /A
 
 	ECHO Restarting Services

5294a0ea4eb655308d25a7147ffb9ce0ad2c40a6
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-10-22 14:01:35 -0500

    One more merge fix

--- Shared/Model/PowerOfAttorney.cs
+++ Shared/Model/PowerOfAttorney.cs
@@ -30,8 +30,6 @@ public partial class PowerOfAttorney
 
     public string Status { get; set; } = null!;
 
-    public Guid? ImagingId { get; set; }
-
     public virtual Agency Agency { get; set; } = null!;
 
     public virtual Insurer Insurer { get; set; } = null!;

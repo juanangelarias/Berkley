eec2a7b491399424cf992d0fda95163b4135687c
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-10-29 14:53:05 -0500

    Upload control working Dragging files onto the window and missing the RadzenUpload control no longer has browser trying to open the file.

---
+++ JamesWebUI/Client/Shared/InlineImagingDocument.razor.js
@@ -0,0 +1,19 @@
+﻿//This file is used to prevent files from being dropped onto the browser except onto the RadzenUpload control
+
+function ParentWithClass(element, classname) {
+    if (element.classList && element.classList.contains(classname)) return true;
+    return element.parentNode && ParentWithClass(element.parentNode, classname);
+}
+
+let dropzoneClass = 'rz-fileupload'
+
+const dragEventHandler = e => {
+    var isInsideDropzone = ParentWithClass(e.target, dropzoneClass)
+    if (isInsideDropzone == undefined || isInsideDropzone === false) {
+        e.preventDefault();
+        e.dataTransfer.effectAllowed = 'none';
+        e.dataTransfer.dropEffect = 'none';
+    }
+}
+
+['dragenter', 'dragover', 'drop'].forEach(ev => window.addEventListener(ev, dragEventHandler, false));

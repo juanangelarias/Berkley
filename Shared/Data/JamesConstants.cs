cfc9fe3ed3522582f1346ce30a3f17c4782346ca
Greg Wilson <SvdSinner@users.noreply.github.com>
2024-12-12 12:25:33 -0600

    Moved SharedLoggingCategories to JamesConstants.cs

--- Shared/Data/JamesConstants.cs
+++ Shared/Data/JamesConstants.cs
@@ -1,8 +1,25 @@
 ﻿namespace James.Shared.Data
 {
+    /// <summary>
+    /// Constants used by multiple assemblies
+    /// </summary>
     public static class JamesConstants
     {
         public const string LOG_IN_PATH = "/account/Login";
         public const string LOG_OUT_PATH = "/account/Logout";
+    }
+
+    /// <summary>
+    /// A set of constants to use as categories for log messages to keep them consistent
+    /// </summary>
+    public static class StandardLoggingCategories
+    {
+        public const string Imaging = "Imaging";
+        public const string UserInterface = "UI";
+        public const string Account = "Account";
+        public const string Agency = "Agency";
+        public const string Obligee = "Obligee";
+        public const string Bond = "Bond";
+        public const string DataAccess = "Data Access";
     }
 }

8631802f2a0ff859d61ce620dfd4e4b2b8185cd0
Bryan Ulfers <bulfers@berkleysurety.com>
2025-03-18 15:46:06 -0500

    BSG-648 Finished implementation of PositionSavingTabs

---
+++ JamesWebUI/Client/Components/PositionSavingTabsItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using Radzen.Blazor;
+
+namespace JamesWebUI.Client.Components
+{
+    public partial class PositionSavingTabsItem : RadzenTabsItem
+    {
+
+
+        [Parameter]
+        public string TabName { get; set; } = "";
+        [Parameter]
+        public EventCallback<string> OnSelected { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            
+            await ((PositionSavingTabs)Tabs).AddTab(this);
+        }
+    }
+}

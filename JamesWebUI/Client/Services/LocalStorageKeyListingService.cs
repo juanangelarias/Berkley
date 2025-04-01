fec52c471c8770cd79ae4cd384808227b3dadb00
Greg Wilson <SvdSinner@users.noreply.github.com>
2025-04-01 15:10:10 -0500

    New services for address and phone formatting and listing Local Storage keys

---
+++ JamesWebUI/Client/Services/LocalStorageKeyListingService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+namespace JamesWebUI.Client.Services
+{
+    /// <summary>
+    /// This service exists because Blazored.LocalStorage is missing this functionality.
+    /// </summary>
+    /// <param name="js">JavaScript runtime reference</param>
+    /// <remarks>If they ever implement this functionality, get rid of this and use that</remarks>
+    public class LocalStorageKeyListingService(IJSRuntime js)
+    {
+        public async Task<string[]> GetAllLocalStorageKeys(string startsWith = "")
+        {
+            var allKeys = await js.InvokeAsync<string[]>("getAllLocalStorageKeys");
+            if (null! == allKeys)
+                return Array.Empty<string>();
+            if (string.IsNullOrEmpty(startsWith))
+                return allKeys;
+            return allKeys.Where(k => k.StartsWith(startsWith)).ToArray();
+        }
+    }
+}
+

4b8857f869351360ee490374cc0d2d19eb89b258
juarias <jarias@berkleysurety.com>
2025-06-02 14:06:35 -0500

    BSG-803
    Ensure nullable safety, refactor data loading logic with caching, and improve error handling in ObligeeMerge, PowerOfAttorneyDocumentStatusCard, and PositionSavingTabs components.

--- JamesWebUI/Client/Components/PositionSavingTabs.cs
+++ JamesWebUI/Client/Components/PositionSavingTabs.cs
@@ -1,16 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
-using Microsoft.JSInterop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Radzen;
 using Radzen.Blazor;
 using Microsoft.AspNetCore.WebUtilities;
-using Blazorise;
-using System.Runtime.CompilerServices;
 
 namespace JamesWebUI.Client.Components
 {

cf08de4ba77653cb6ec076958b45ac90c852ac3b
juarias <jarias@berkleysurety.com>
2025-06-05 08:36:38 -0500

    BSG-798
    Initializing AccountNum with null! instead of empty string

--- Shared/Model/InforceAccountLOA.cs
+++ Shared/Model/InforceAccountLOA.cs
@@ -8,7 +8,7 @@ namespace James.Shared.Model
 {
     public partial class InforceAccountLOA
     {
-        public string AccountNum { get; set; } = string.Empty;
+        public string AccountNum { get; set; } = null!;
         public LineOfAuthorityLog? ContractLOA { get; set; }
         public LineOfAuthorityLog? CommercialLOA { get; set; }
     }

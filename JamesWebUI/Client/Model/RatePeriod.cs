3bc689544a71ddb29cb355898fe71a473c194b3e
juarias <jarias@berkleysurety.com>
2025-08-11 07:51:19 -0500

    Add `RatePeriod` and `RateRange` models; refactor and reorganize `Agency` components into a dedicated folder for improved structure. Simplify `AgencyCommissionRates` caching logic.

---
+++ JamesWebUI/Client/Model/RatePeriod.cs
@@ -0,0 +1,26 @@
+﻿using James.Shared;
+
+namespace JamesWebUI.Client.Model;
+
+public class RatePeriod: NotifyPropertyChanged
+{
+    #region Start
+
+    private DateOnly _start;
+    private DateOnly? _originalStart;
+
+    public DateOnly Start
+    {
+        get => _start;
+        set
+        {
+            _start = value;
+            OnPropertyChanged();
+        }
+    }
+
+    #endregion
+    
+    public DateOnly Finish { get; set; }
+    public List<RateRange> Rates { get; set; } = [];
+}
---
+++ JamesWebUI/Client/Model/RateRange.cs
@@ -0,0 +1,8 @@
+﻿namespace JamesWebUI.Client.Model;
+
+public class RateRange
+{
+    public int From { get; set; }
+    public int To { get; set; }
+    public double Rate { get; set; }
+}

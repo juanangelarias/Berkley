9984436dc03168768a3cfbe326737e1222fa8fb1
Greg Wilson <SvdSinner@users.noreply.github.com>
2025-09-09 12:08:33 -0500

    Fixing merge issue

--- Shared/Data/IDataAccess.cs
+++ Shared/Data/IDataAccess.cs
@@ -8,7 +8,7 @@ namespace James.Shared.Data
         #region Data Caching functionality from BaseDataAccess
 
         public IBrowserStorageCache BrowserStorageCache { get; }
-        public Task GetCacheOrLoadDataAsync(LoadItem loadItem, ILoggingService logger);
+        //public Task GetCacheOrLoadDataAsync(LoadItem loadItem, ILoggingService logger);
         public Task GetCacheOrLoadDataAsync(LoadItem loadItem);
 
         /// <summary>

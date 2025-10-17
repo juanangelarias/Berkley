4f49e83033532ed2b7363838564af71ee81e149c
juarias <jarias@berkleysurety.com>
2025-10-17 07:31:58 -0500

    Enhance: add `GetUserSettings` GraphQL query with authorization; refine header styling in `MainLayout` for consistency; update `RelatedPartiesLoad` key in `AgencyLocationsGrid`; remove unused `agencyTopParentId` from schema.

---
+++ Data/JamesDAL.Server/GraphQL/Queries/UserSettings.cs
@@ -0,0 +1,19 @@
+﻿using HotChocolate.Authorization;
+
+namespace James.Data.Server.GraphQL.Queries;
+
+public partial class Query
+{
+    [Authorize]
+    public async Task<UserSetting?> GetUserSettings(string userEmail, string key,
+        [Service] IDbContextFactory<JamesDatabaseContext> contextFactory)
+    {
+        var ctx = await contextFactory.CreateDbContextAsync();
+
+        var settings = await ctx.UserSettings
+            .Where(r => r.Username == userEmail && r.Key == key)
+            .FirstOrDefaultAsync();
+
+        return settings;
+    }
+}

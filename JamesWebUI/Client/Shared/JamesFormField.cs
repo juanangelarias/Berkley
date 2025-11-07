f3b57ef5737d6125cf26bf60a12d9e8129b610f0
juarias <jarias@berkleysurety.com>
2025-11-07 15:03:02 -0600

    Fixing Unit Test

--- JamesWebUI/Client/Shared/JamesFormField.cs
+++ JamesWebUI/Client/Shared/JamesFormField.cs
@@ -12,13 +12,60 @@ public sealed class JamesFormField : RadzenFormField
         get => _style;
         set
         {
-            _style = string.IsNullOrEmpty(value)
-                ? _style = "width: 100%"
-                : value.Contains("width")
-                    ? _style = value
-                    : value[value.Length - 1] == ';'
-                        ? $"{value} width: 100%;"
-                        : $"{value}; width: 100%;";
+            if (string.IsNullOrEmpty(value))
+            {
+                _style = "width: 100%";
+                return;
+            }
+
+            if (value.Contains("width") &&
+                !value.Contains("min-width") &&
+                !value.Contains("max-width"))
+            {
+                _style = value;
+                return;
+            }
+
+            var count = 0;
+            var index = 0;
+            while ((index = value.IndexOf("width", index, StringComparison.Ordinal)) != -1) 
+            {
+                count++;
+                index += "width".Length;
+            }
+
+            if (count == 0)
+            {
+                _style = value[value.Length - 1] == ';'
+                    ? _style = $"{value} width: 100%;"
+                    : $"{value}; width: 100%;";
+                return;
+            }
+
+            if (count == 1 && (value.Contains("min-width") || value.Contains("max-width")))
+            {
+                _style = value[value.Length - 1] == ';'
+                    ? _style = $"{value} width: 100%;"
+                    : $"{value}; width: 100%;";
+                return;
+            }
+            
+            if (count == 1 && !value.Contains("min-width") && !value.Contains("max-width"))
+            {
+                _style = value;
+                return;           
+            }
+            
+            if (count == 2 && value.Contains("min-width") && value.Contains("max-width"))
+            {
+                _style = value[value.Length - 1] == ';'
+                    ? _style = $"{value} width: 100%;"
+                    : $"{value}; width: 100%;";
+            }
+            else
+            {
+                _style = value;
+            }
         }
     }
 
--- Tests/JamesWebUI.Client.Test/JamesFormFieldTests.cs
+++ Tests/JamesWebUI.Client.Test/JamesFormFieldTests.cs
@@ -57,5 +57,126 @@ namespace JamesWebUI.Client.Test
             Assert.True(contains100);
             Assert.True(containsColor);
         }
+        
+        [Fact]
+        public void Constructor_Sets_User_Style_WithoutWidth_WithMinAndMaxWidth()
+        {
+            // Act
+            var field = new JamesFormField
+            {
+                Style = "color:red; min-width: 100px; max-width: 200px;"
+            };
+
+            var containsMin = field.Style.Contains("min-width: 100px");
+            var containsMax = field.Style.Contains("max-width: 200px");
+            var containsColor = field.Style.Contains("color:red");
+            var containsWidth100 = field.Style.Contains("width: 100%");
+            // Assert
+            Assert.True(containsMin);
+            Assert.True(containsMax);
+            Assert.True(containsColor);
+            Assert.True(containsWidth100);
+        }
+        
+        [Fact]
+        public void Constructor_Sets_User_Style_WithoutWidth_WithMinWidth()
+        {
+            // Act
+            var field = new JamesFormField
+            {
+                Style = "color:red; min-width: 100px"
+            };
+
+            var containsMin = field.Style.Contains("min-width: 100px");
+            var containsMax = field.Style.Contains("max-width: 200px");
+            var containsColor = field.Style.Contains("color:red");
+            var containsWidth100 = field.Style.Contains("width: 100%");
+            // Assert
+            Assert.True(containsMin);
+            Assert.False(containsMax);
+            Assert.True(containsColor);
+            Assert.True(containsWidth100);
+        }
+        
+        [Fact]
+        public void Constructor_Sets_User_Style_WithoutWidth_WithMaxWidth()
+        {
+            // Act
+            var field = new JamesFormField
+            {
+                Style = "color:red; max-width: 200px;"
+            };
+
+            var containsMin = field.Style.Contains("min-width: 100px");
+            var containsMax = field.Style.Contains("max-width: 200px");
+            var containsColor = field.Style.Contains("color:red");
+            var containsWidth100 = field.Style.Contains("width: 100%");
+            // Assert
+            Assert.False(containsMin);
+            Assert.True(containsMax);
+            Assert.True(containsColor);
+            Assert.True(containsWidth100);
+        }
+        
+        // With
+        [Fact]
+        public void Constructor_Sets_User_Style_WithWidth_WithMinAndMaxWidth()
+        {
+            // Act
+            var field = new JamesFormField
+            {
+                Style = "color:red; min-width: 100px; max-width: 200px; width: 75%;"
+            };
+
+            var containsMin = field.Style.Contains("min-width: 100px");
+            var containsMax = field.Style.Contains("max-width: 200px");
+            var containsColor = field.Style.Contains("color:red");
+            var containsWidth75 = field.Style.Contains("width: 75%");
+            // Assert
+            Assert.True(containsMin);
+            Assert.True(containsMax);
+            Assert.True(containsColor);
+            Assert.True(containsWidth75);
+        }
+        
+        [Fact]
+        public void Constructor_Sets_User_Style_WithWidth_WithMinWidth()
+        {
+            // Act
+            var field = new JamesFormField
+            {
+                Style = "color:red; min-width: 100px; width: 75%;"
+            };
+
+            var containsMin = field.Style.Contains("min-width: 100px");
+            var containsMax = field.Style.Contains("max-width: 200px");
+            var containsColor = field.Style.Contains("color:red");
+            var containsWidth75 = field.Style.Contains("width: 75%");
+            // Assert
+            Assert.True(containsMin);
+            Assert.False(containsMax);
+            Assert.True(containsColor);
+            Assert.True(containsWidth75);
+        }
+        
+        [Fact]
+        public void Constructor_Sets_User_Style_WithWidth_WithMaxWidth()
+        {
+            // Act
+            var field = new JamesFormField
+            {
+                Style = "color:red; max-width: 200px; width: 75%;"
+            };
+
+            var containsMin = field.Style.Contains("min-width: 100px");
+            var containsMax = field.Style.Contains("max-width: 200px");
+            var containsColor = field.Style.Contains("color:red");
+            var containsWidth75 = field.Style.Contains("width: 75%");
+            // Assert
+            Assert.False(containsMin);
+            Assert.True(containsMax);
+            Assert.True(containsColor);
+            Assert.True(containsWidth75);
+        }
     }
 }

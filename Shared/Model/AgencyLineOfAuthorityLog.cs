8fdab94f927fd676d4c0e77a512e0da0c5ba2c6f
Greg Wilson <gwilson@berkleysurety.com>
2025-11-18 14:15:09 -0600

    November 2025 Schema changes

---
+++ Shared/Model/AgencyLineOfAuthorityLog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace James.Shared.Model;
+//Generated for DB
+
+public partial class AgencyLineOfAuthorityLog
+{
+    public Guid Id { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public DateTime Modified { get; set; }
+
+    public string AgencyNumber { get; set; } = null!;
+
+    public string AccountNum { get; set; } = null!;
+
+    public int SequenceNumber { get; set; }
+
+    public DateTime Effective { get; set; }
+
+    public DateTime Expiration { get; set; }
+
+    public int Loasingle { get; set; }
+
+    public int Loaaggregate { get; set; }
+
+    public string? Comments { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? Division { get; set; }
+
+    public string? BondType { get; set; }
+
+    public Guid? CreatedBy { get; set; }
+
+    public string? Conditions { get; set; }
+
+    public virtual ICollection<AgencyLineOfAuthorityAgent> AgencyLineOfAuthorityAgents { get; set; } = new List<AgencyLineOfAuthorityAgent>();
+
+    public virtual UserProfile? CreatedByNavigation { get; set; }
+
+    public virtual LineOfAuthorityStatusDm? StatusNavigation { get; set; }
+}

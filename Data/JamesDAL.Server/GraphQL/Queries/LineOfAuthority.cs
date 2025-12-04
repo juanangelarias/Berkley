5e9a4909b4fa3b1c7e35f8746c42fbd70b545856
juarias <jarias@berkleysurety.com>
2025-12-04 09:23:40 -0600

    Added `GetLoaLogsByAccount` query to retrieve Line of Authority logs based on account number. Implemented authorization and database context handling.

---
+++ Data/JamesDAL.Server/GraphQL/Queries/LineOfAuthority.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace James.Data.Server.GraphQL.Queries;
+
+public partial class Query
+{
+    [Authorize]
+    public async Task<List<LineOfAuthorityLog>> GetLoaLogsByAccount(string accountNumber,
+        [Service] IDbContextFactory<JamesDatabaseContext> contextFactory)
+    {
+        var ctx = await contextFactory.CreateDbContextAsync();
+        
+        var response = await ctx.LineOfAuthorityLogs
+            .Where(l => l.AccountNum == accountNumber)
+            .ToListAsync();
+        
+        return response;
+    }
+    
+    
+}

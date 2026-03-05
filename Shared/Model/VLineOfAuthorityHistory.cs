1678f16d57e49f280aca01752731d0d94039df19
Greg Wilson <gwilson@berkleysurety.com>
2026-03-05 14:25:30 -0600

    Changes for Juan

---
+++ Shared/Model/VLineOfAuthorityHistory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace James.Shared.Model;
+//Generated for DB
+
+public partial class VLineOfAuthorityHistory
+{
+    public Guid Id { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public DateTime Modified { get; set; }
+
+    public string AccountNum { get; set; } = null!;
+
+    public int SequenceNumber { get; set; }
+
+    public DateTime Effective { get; set; }
+
+    public DateTime Expiration { get; set; }
+
+    public int Loasingle { get; set; }
+
+    public int? PreviousSingle { get; set; }
+
+    public int Loaaggregate { get; set; }
+
+    public int? PreviousAggregate { get; set; }
+
+    public string? Comments { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? PreviousStatus { get; set; }
+
+    public Guid? ApprovedBy { get; set; }
+
+    public DateTime? Approved { get; set; }
+
+    public string? Division { get; set; }
+
+    public string? BondType { get; set; }
+
+    public string? Conditions { get; set; }
+
+    public bool HomeOfficeApproved { get; set; }
+
+    public Guid? CreatedBy { get; set; }
+}

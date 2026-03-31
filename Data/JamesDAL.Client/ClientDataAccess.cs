3e292868a7886bf1236322f0425dacdf8ba2193a
juarias <jarias@berkleysurety.com>
2026-03-31 10:45:05 -0500

    Implemented `GetCreditReport` in `ClientDataAccess.cs` to fetch credit report data using `ExecuteGet`.

--- Data/JamesDAL.Client/ClientDataAccess.cs
+++ Data/JamesDAL.Client/ClientDataAccess.cs
@@ -647,11 +647,10 @@ namespace James.Data.Client
 
         public async Task<IDataAccessResult<List<CreditReportHistory>>> GetCreditReport(string accountNum)
         {
-            throw new NotImplementedException();
-            /*var result = await ExecuteGet<CreditReportDto>(async ()=>
+            var result = await ExecuteGet<List<CreditReportHistory>>(async ()=>
                 await jamesClient.GetCreditReport.ExecuteAsync(accountNum), "CreditReport");
 
-            return result;*/
+            return result;
         }
 
         public async Task<IDataAccessResult<List<CreditReportDm>>> GetCreditReportAgencies()
